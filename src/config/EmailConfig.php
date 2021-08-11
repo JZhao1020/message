e284874e109b9d78e142b8d1c71b75e6f94f0455
hao <18816784184@163.com>
2021-08-11 19:02:28 +0800

    Update EmailConfig.php

--- src/config/EmailConfig.php
+++ src/config/EmailConfig.php
@@ -2,10 +2,10 @@
 return [
     // 邮件配置
 //    'host' => 'smtp.exmail.qq.com',				//smtp服务器地址
-//    'user' => 'service@fenzis.com',			//发送账号
-//    'pwd'  => 'Adg409926',						//发送密码
-//    'name' => 'Hyper Talk',				//发送人昵称
-//    'system' => 'service@fenzis.com',				//发送人昵称
+//    'user' => '',			//发送账号
+//    'pwd'  => '',						//发送密码
+//    'name' => '',				//发送人昵称
+//    'system' => '',				//发送人昵称
 //    'port' => 465,                                                  //端口
 //    'secure' => 'ssl',                                              //协议
 //    'debug'=> false,								//开发模式
